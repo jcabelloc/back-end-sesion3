44232b98a2738b6c47c924005ad5bb374eae3465
Juan Cabello <jcabelloc@itana.pe>
2024-10-11 19:55:45 -0500

    Uso de body-parser

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-const express = require("express");
+const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
 
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
 app.use('/crear-producto', (req, res, next) => {
     console.log('Estamos en el MiddleWare');
-    res.send('<h1>Vamos a crear un producto</h1>');
+    res.send('<form action="/productos" method="POST"><input type="text" name="nombreproducto"><button type="submit">Crear</button></form>');
+});
+
+app.use('/productos', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/')
 });
 
 app.use('/', (req, res, next) => {
