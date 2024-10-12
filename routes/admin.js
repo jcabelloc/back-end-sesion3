933627616fbd8cc5f5d11cd96bbf7452578240f4
Juan Cabello <jcabelloc@itana.pe>
2024-10-11 20:20:22 -0500

    Uso de filtro '/admin'

--- routes/admin.js
+++ routes/admin.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
+// /admin/crear-producto
 router.get('/crear-producto', (req, res, next) => {
-    res.send('<form action="/productos" method="POST"><input type="text" name="nombreproducto"><button type="submit">Crear</button></form>');
+    res.send('<form action="/admin/productos" method="POST"><input type="text" name="nombreproducto"><button type="submit">Crear</button></form>');
 });
 
+// /admin/productos
 router.post('/productos', (req, res, next) => {
     console.log(req.body);
     res.redirect('/')
