e1a3290fc563137c13c21105c5c869f3d1781325
Juan Cabello <jcabelloc@itana.pe>
2024-10-11 20:47:53 -0500

    Explicacion sobre el uso de middleware y req, res y next

--- app.js
+++ app.js
@@ -15,8 +15,13 @@ app.use(tiendaRoutes);
 
 app.use((req, res, next) => {
     // res.status(404).send('<h1>PAGINA NO ENCONTRADA</h1>');
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    console.log('Viendo que URL el usuario inhresa pero no existe')
+    console.log(req.url);
+    next();
+});
 
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
 app.listen(3000);
