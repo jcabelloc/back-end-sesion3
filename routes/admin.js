4c7d055b703a279e69be5ab089057b29004e679f
Juan Cabello <jcabelloc@itana.pe>
2024-10-11 20:38:23 -0500

    Uso de utilitario para navegar a la raiz

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,17 @@
 const path = require('path');
 const express = require('express');
 
+const raizDir = require('../utils/path');
+
+
 const router = express.Router();
 
 // /admin/crear-producto
 router.get('/crear-producto', (req, res, next) => {
     // res.send('<form action="/admin/productos" method="POST"><input type="text" name="nombreproducto"><button type="submit">Crear</button></form>');
-    res.sendFile(path.join(__dirname, '..', 'views', 'crear-producto.html'));
+    //res.sendFile(path.join(__dirname, '..', 'views', 'crear-producto.html'));
+    res.sendFile(path.join(raizDir, 'views', 'crear-producto.html'));
+
 });
 
 // /admin/productos
